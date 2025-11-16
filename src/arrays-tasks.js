c7780492aeeb355a1f9f44d7370c571face24eec
Vita Kesso <buvitabu@gmail.com>
2025-11-16 22:57:09 +0300

    feat: 1 & 2

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,9 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  const length = end - start + 1;
+  return Array.from({ length }, (_, index) => start + index);
 }
 
 /**
@@ -37,8 +38,10 @@ function getIntervalArray(/* start, end */) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const big = arr1.length > arr2.length ? arr1 : arr2;
+  const small = arr1 === big ? arr2 : arr1;
+  return big.map((element, index) => element + (small[index] || 0));
 }
 
 /**
